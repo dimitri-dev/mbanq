1f3bcd400f7a6d4131a73d54d373d02a39c335f5
dimitri-dev <maric.neven03@gmail.com>
2023-02-11 11:36:03 +0100

    Algorithms #3 - implemented a solution for larger number divisible by 3

---
+++ Mbanq/Algorithms/FirstLargerNumberDivisibleByThree/FirstLargerNumberDivisibleByThree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mbanq.Algorithms
+{
+    public class FirstLargerNumberDivisibleByThree
+    {
+        // This is a safe solution, if number is out of bounds (i.e. too large, it will return 0)
+        // Logic: If at any point "a" is MaxValue, return 0 since next inrementation will be MinValue, which isn't "larger".
+        // Different solution: Use BigInteger from System.Numerics - then you are memory bound.
+        public long GetsFirstLargerNumberDivisibleByThree(long a)
+        {
+            do
+            {
+                if (a == long.MaxValue)
+                    return 0;
+
+                ++a;
+            } while (a % 3 != 0);
+
+            return a;
+        }
+    }
+}
