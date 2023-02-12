93090bfd6cd0c99aa821afc4b87a5613551bf441
dimitri-dev <maric.neven03@gmail.com>
2023-02-13 00:27:44 +0100

    Persons - added string extension method for validation

---
+++ Mbanq/PersonManagement.Common/Helpers/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonManagement.Common
+{
+    public static class Extensions
+    {
+        public static bool IsNullOrEmpty(this string str)
+        {
+            if (str == null || str.Length == 0) return true;
+            else return false;
+        }
+    }
+}
