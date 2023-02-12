9b5a878ddbe47b8368a3b14b587e4bbae74e3c28
dimitri-dev <maric.neven03@gmail.com>
2023-02-12 14:10:08 +0100

    Persons - WinForms - implemented base person form functionality with Infragistics UI

---
+++ Mbanq/PersonManagement/PersonForm.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace PersonManagement
+{
+    public partial class PersonForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Infragistics.Win.Appearance appearance1 = new Infragistics.Win.Appearance();
+            Infragistics.Win.UltraWinGrid.UltraGridBand ultraGridBand1 = new Infragistics.Win.UltraWinGrid.UltraGridBand("Person", -1);
+            Infragistics.Win.UltraWinGrid.UltraGridColumn ultraGridColumn21 = new Infragistics.Win.UltraWinGrid.UltraGridColumn("Id");
+            Infragistics.Win.UltraWinGrid.UltraGridColumn ultraGridColumn22 = new Infragistics.Win.UltraWinGrid.UltraGridColumn("OIB");
+            Infragistics.Win.UltraWinGrid.UltraGridColumn ultraGridColumn23 = new Infragistics.Win.UltraWinGrid.UltraGridColumn("Name");
+            Infragistics.Win.UltraWinGrid.UltraGridColumn ultraGridColumn24 = new Infragistics.Win.UltraWinGrid.UltraGridColumn("Surname");
+            Infragistics.Win.UltraWinGrid.UltraGridColumn ultraGridColumn25 = new Infragistics.Win.UltraWinGrid.UltraGridColumn("Place");
+            Infragistics.Win.UltraWinGrid.UltraGridColumn ultraGridColumn26 = new Infragistics.Win.UltraWinGrid.UltraGridColumn("Address");
+            Infragistics.Win.UltraWinGrid.UltraGridColumn ultraGridColumn27 = new Infragistics.Win.UltraWinGrid.UltraGridColumn("Phone");
+            Infragistics.Win.UltraWinGrid.UltraGridColumn ultraGridColumn28 = new Infragistics.Win.UltraWinGrid.UltraGridColumn("Mail");
+            Infragistics.Win.UltraWinGrid.UltraGridColumn ultraGridColumn29 = new Infragistics.Win.UltraWinGrid.UltraGridColumn("DateCreated");
+            Infragistics.Win.UltraWinGrid.UltraGridColumn ultraGridColumn30 = new Infragistics.Win.UltraWinGrid.UltraGridColumn("DateUpdated");
+            Infragistics.Win.Appearance appearance2 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance3 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance4 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance5 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance6 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance7 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance8 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance9 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance10 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance11 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance12 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance13 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance14 = new Infragistics.Win.Appearance();
+            Infragistics.Win.Appearance appearance15 = new Infragistics.Win.Appearance();
+            this.ultraGrid1 = new Infragistics.Win.UltraWinGrid.UltraGrid();
+            this.mbanqDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.mbanqDataSet = new PersonManagement.MbanqDataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.ultraGrid1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mbanqDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mbanqDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ultraGrid1
+            // 
+            this.ultraGrid1.CausesValidation = false;
+            this.ultraGrid1.DataMember = "Person";
+            this.ultraGrid1.DataSource = this.mbanqDataSetBindingSource;
+            appearance1.BackColor = System.Drawing.Color.WhiteSmoke;
+            appearance1.BackColor2 = System.Drawing.Color.SlateBlue;
+            appearance1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ultraGrid1.DisplayLayout.Appearance = appearance1;
+            this.ultraGrid1.DisplayLayout.AutoFitStyle = Infragistics.Win.UltraWinGrid.AutoFitStyle.ResizeAllColumns;
+            ultraGridColumn21.AllowGroupBy = Infragistics.Win.DefaultableBoolean.False;
+            ultraGridColumn21.Header.VisiblePosition = 0;
+            ultraGridColumn21.Hidden = true;
+            ultraGridColumn21.Width = 446;
+            ultraGridColumn22.Header.VisiblePosition = 1;
+            ultraGridColumn22.Width = 142;
+            ultraGridColumn23.Header.VisiblePosition = 2;
+            ultraGridColumn23.Width = 148;
+            ultraGridColumn24.Header.VisiblePosition = 3;
+            ultraGridColumn24.Width = 132;
+            ultraGridColumn25.Header.VisiblePosition = 4;
+            ultraGridColumn25.Width = 154;
+            ultraGridColumn26.Header.VisiblePosition = 5;
+            ultraGridColumn26.Width = 143;
+            ultraGridColumn27.Header.VisiblePosition = 6;
+            ultraGridColumn27.Width = 145;
+            ultraGridColumn28.Header.VisiblePosition = 7;
+            ultraGridColumn28.Width = 142;
+            ultraGridColumn29.Header.VisiblePosition = 8;
+            ultraGridColumn29.Hidden = true;
+            ultraGridColumn29.Width = 226;
+            ultraGridColumn30.Header.VisiblePosition = 9;
+            ultraGridColumn30.Hidden = true;
+            ultraGridColumn30.Width = 261;
+            ultraGridBand1.Columns.AddRange(new object[] {
+            ultraGridColumn21,
+            ultraGridColumn22,
+            ultraGridColumn23,
+            ultraGridColumn24,
+            ultraGridColumn25,
+            ultraGridColumn26,
+            ultraGridColumn27,
+            ultraGridColumn28,
+            ultraGridColumn29,
+            ultraGridColumn30});
+            appearance2.BackColor = System.Drawing.Color.SlateBlue;
+            appearance2.FontData.BoldAsString = "True";
+            ultraGridBand1.Header.Appearance = appearance2;
+            this.ultraGrid1.DisplayLayout.BandsSerializer.Add(ultraGridBand1);
+            this.ultraGrid1.DisplayLayout.BorderStyle = Infragistics.Win.UIElementBorderStyle.Solid;
+            this.ultraGrid1.DisplayLayout.CaptionVisible = Infragistics.Win.DefaultableBoolean.False;
+            this.ultraGrid1.DisplayLayout.DefaultSelectedBackColor = System.Drawing.Color.Purple;
+            appearance3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            appearance3.BackColor2 = System.Drawing.SystemColors.ControlDark;
+            appearance3.BackGradientStyle = Infragistics.Win.GradientStyle.Vertical;
+            appearance3.BorderColor = System.Drawing.SystemColors.Window;
+            this.ultraGrid1.DisplayLayout.GroupByBox.Appearance = appearance3;
+            appearance4.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.ultraGrid1.DisplayLayout.GroupByBox.BandLabelAppearance = appearance4;
+            this.ultraGrid1.DisplayLayout.GroupByBox.BorderStyle = Infragistics.Win.UIElementBorderStyle.Solid;
+            this.ultraGrid1.DisplayLayout.GroupByBox.Hidden = true;
+            appearance5.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            appearance5.BackColor2 = System.Drawing.SystemColors.Control;
+            appearance5.BackGradientStyle = Infragistics.Win.GradientStyle.Horizontal;
+            appearance5.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.ultraGrid1.DisplayLayout.GroupByBox.PromptAppearance = appearance5;
+            this.ultraGrid1.DisplayLayout.MaxColScrollRegions = 1;
+            this.ultraGrid1.DisplayLayout.MaxRowScrollRegions = 1;
+            appearance6.BackColor = System.Drawing.SystemColors.Window;
+            appearance6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ultraGrid1.DisplayLayout.Override.ActiveCellAppearance = appearance6;
+            appearance7.BackColor = System.Drawing.Color.SlateBlue;
+            appearance7.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.ultraGrid1.DisplayLayout.Override.ActiveRowAppearance = appearance7;
+            this.ultraGrid1.DisplayLayout.Override.AllowAddNew = Infragistics.Win.UltraWinGrid.AllowAddNew.Yes;
+            this.ultraGrid1.DisplayLayout.Override.AllowColMoving = Infragistics.Win.UltraWinGrid.AllowColMoving.NotAllowed;
+            this.ultraGrid1.DisplayLayout.Override.AllowDelete = Infragistics.Win.DefaultableBoolean.True;
+            this.ultraGrid1.DisplayLayout.Override.AllowRowFiltering = Infragistics.Win.DefaultableBoolean.False;
+            this.ultraGrid1.DisplayLayout.Override.AllowRowSummaries = Infragistics.Win.UltraWinGrid.AllowRowSummaries.False;
+            this.ultraGrid1.DisplayLayout.Override.AllowUpdate = Infragistics.Win.DefaultableBoolean.True;
+            this.ultraGrid1.DisplayLayout.Override.BorderStyleCell = Infragistics.Win.UIElementBorderStyle.RaisedSoft;
+            this.ultraGrid1.DisplayLayout.Override.BorderStyleRow = Infragistics.Win.UIElementBorderStyle.Dashed;
+            appearance8.BackColor = System.Drawing.Color.Transparent;
+            this.ultraGrid1.DisplayLayout.Override.CardAreaAppearance = appearance8;
+            appearance9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ultraGrid1.DisplayLayout.Override.CellAppearance = appearance9;
+            this.ultraGrid1.DisplayLayout.Override.CellClickAction = Infragistics.Win.UltraWinGrid.CellClickAction.EditAndSelectText;
+            this.ultraGrid1.DisplayLayout.Override.CellPadding = 0;
+            this.ultraGrid1.DisplayLayout.Override.CellSpacing = 3;
+            appearance10.BackColor = System.Drawing.SystemColors.Control;
+            appearance10.BackColor2 = System.Drawing.SystemColors.ControlDark;
+            appearance10.BackGradientAlignment = Infragistics.Win.GradientAlignment.Element;
+            appearance10.BackGradientStyle = Infragistics.Win.GradientStyle.Horizontal;
+            appearance10.BorderColor = System.Drawing.SystemColors.Window;
+            this.ultraGrid1.DisplayLayout.Override.GroupByRowAppearance = appearance10;
+            appearance11.BackColor = System.Drawing.Color.SlateBlue;
+            appearance11.BackGradientStyle = Infragistics.Win.GradientStyle.None;
+            appearance11.FontData.BoldAsString = "True";
+            appearance11.FontData.Name = "Trebuchet MS";
+            appearance11.FontData.SizeInPoints = 9F;
+            appearance11.ForeColor = System.Drawing.Color.Black;
+            appearance11.ThemedElementAlpha = Infragistics.Win.Alpha.Transparent;
+            this.ultraGrid1.DisplayLayout.Override.HeaderAppearance = appearance11;
+            this.ultraGrid1.DisplayLayout.Override.HeaderClickAction = Infragistics.Win.UltraWinGrid.HeaderClickAction.SortMulti;
+            this.ultraGrid1.DisplayLayout.Override.HeaderStyle = Infragistics.Win.HeaderStyle.WindowsXPCommand;
+            appearance12.BackColor = System.Drawing.Color.Transparent;
+            this.ultraGrid1.DisplayLayout.Override.RowAppearance = appearance12;
+            appearance13.BackColor = System.Drawing.Color.Transparent;
+            this.ultraGrid1.DisplayLayout.Override.RowSelectorAppearance = appearance13;
+            this.ultraGrid1.DisplayLayout.Override.RowSelectors = Infragistics.Win.DefaultableBoolean.False;
+            this.ultraGrid1.DisplayLayout.Override.RowSpacingAfter = 4;
+            this.ultraGrid1.DisplayLayout.Override.RowSpacingBefore = 4;
+            appearance14.BackColor = System.Drawing.Color.CornflowerBlue;
+            appearance14.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            appearance14.BackGradientStyle = Infragistics.Win.GradientStyle.Horizontal;
+            this.ultraGrid1.DisplayLayout.Override.SelectedRowAppearance = appearance14;
+            this.ultraGrid1.DisplayLayout.Override.SelectTypeRow = Infragistics.Win.UltraWinGrid.SelectType.Single;
+            this.ultraGrid1.DisplayLayout.Override.SummaryDisplayArea = Infragistics.Win.UltraWinGrid.SummaryDisplayAreas.None;
+            appearance15.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ultraGrid1.DisplayLayout.Override.TemplateAddRowAppearance = appearance15;
+            this.ultraGrid1.DisplayLayout.RowConnectorColor = System.Drawing.Color.SlateBlue;
+            this.ultraGrid1.DisplayLayout.RowConnectorStyle = Infragistics.Win.UltraWinGrid.RowConnectorStyle.Dotted;
+            this.ultraGrid1.DisplayLayout.ScrollBounds = Infragistics.Win.UltraWinGrid.ScrollBounds.ScrollToFill;
+            this.ultraGrid1.DisplayLayout.ScrollStyle = Infragistics.Win.UltraWinGrid.ScrollStyle.Immediate;
+            this.ultraGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ultraGrid1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ultraGrid1.Location = new System.Drawing.Point(0, 0);
+            this.ultraGrid1.Name = "ultraGrid1";
+            this.ultraGrid1.Size = new System.Drawing.Size(1008, 729);
+            this.ultraGrid1.TabIndex = 0;
+            this.ultraGrid1.Text = "ultraGrid1";
+            this.ultraGrid1.UseFlatMode = Infragistics.Win.DefaultableBoolean.True;
+            this.ultraGrid1.UseOsThemes = Infragistics.Win.DefaultableBoolean.True;
+            // 
+            // mbanqDataSetBindingSource
+            // 
+            this.mbanqDataSetBindingSource.DataSource = this.mbanqDataSet;
+            this.mbanqDataSetBindingSource.Position = 0;
+            // 
+            // mbanqDataSet
+            // 
+            this.mbanqDataSet.DataSetName = "MbanqDataSet";
+            this.mbanqDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // PersonForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.ultraGrid1);
+            this.Name = "PersonForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Person Management by Neven Marić - mbanq";
+            ((System.ComponentModel.ISupportInitialize)(this.ultraGrid1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mbanqDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mbanqDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Infragistics.Win.UltraWinGrid.UltraGrid ultraGrid1;
+        private System.Windows.Forms.BindingSource mbanqDataSetBindingSource;
+        private MbanqDataSet mbanqDataSet;
+    }
+}
+
---
+++ Mbanq/PersonManagement/PersonForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonManagement
+{
+    public partial class PersonForm : Form
+    {
+        public PersonForm()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadData()
+        {
+
+        }
+    }
+}
